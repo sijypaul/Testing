b3fe236d73a4d3122e93df6b85a58c36764ea502
sijypaul <104199522+sijypaul@users.noreply.github.com>
2023-07-19 13:26:34 +0530

    Update and rename SumofDigits.c to SumofDigits.java

---
+++ SumofDigits.java
@@ -0,0 +1,29 @@
+// Java program to compute
+// sum of digits in number.
+import java.io.*;
+
+class SumofDigits {
+
+	/* Function to get sum of digits */
+	static int getSum(int n)
+	{
+		int sum;
+
+		/* Single line that calculates sum */
+		for (sum = 0; n > 0; sum += n % 10, n /= 10)
+			;
+
+		return sum;
+	}
+
+	// Driver code
+	public static void main(String[] args)
+	{
+		int n = 687;
+
+		// Function call
+		System.out.println(getSum(n));
+	}
+}
+
+// This code is contributed by Gitanjali
